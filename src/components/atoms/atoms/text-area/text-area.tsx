864c651cc842c14f070ffcc7a4ff0a25131ca20d
mag_d_alena <magdalena-b@noal.org.il>
2023-11-14 10:30:02 +0200

    file cleanup

--- src/components/atoms/atoms/text-area/text-area.tsx
+++
@@ -1,6 +0,0 @@
-import { twMerge } from 'tailwind-merge'
-import { input } from '~/components/electrons/input'
-
-export function TextArea(props: React.HTMLProps<HTMLTextAreaElement>) {
-  return <textarea className={twMerge(input(), 'resize-none rounded-xl')} {...props} />
-}
