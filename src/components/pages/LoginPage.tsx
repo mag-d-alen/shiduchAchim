8f4f25865ef72f87966374b1bb8cda6595c8ea39
mag_d_alena <magdalena-b@noal.org.il>
2023-11-27 17:37:40 +0200

    added backbone to working pages

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,11 +1,12 @@
 import { Button } from "../atoms/Button"
 import { Input } from "../atoms/Input"
+import { Header } from "../common/Header"
 
 export const LoginPage = () => {
   return (
-    <div className=" flex flex-col p-8">
+    <><Header text="login" /><div className=" flex w-1/2 m-auto gap-2 flex-col p-8">
       <Input placeholder={"שם משתמש"} />
       <Input type={"password"} placeholder={"סיסמה"} />
       <Button>{"המשך"}</Button>
-    </div>)
+    </div></>)
 }
